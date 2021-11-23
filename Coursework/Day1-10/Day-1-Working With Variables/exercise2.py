f3547c44c8269fd2df57eacb7361ed62050e1780
Shaun <78476567+SAndertonPyJS@users.noreply.github.com>
2021-11-23 13:11:45 +0000

    Add files via upload
    Added exercises from day 1 + readme with description

---
+++ Coursework/Day1-10/Day-1-Working With Variables/exercise2.py
@@ -0,0 +1,6 @@
+#the corrections required were: missing () line 3, + sign had "" around it which was breaking str line 4,  line 5 was missing '' and line 6 was missing "
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
